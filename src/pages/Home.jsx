f2b4b9727daa6d4dd76d099a06a0104f5f828b83
Liam <liamnguyen224@gmail.com>
2023-11-24 20:19:52 -0800

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,13 @@ export default function Home() {
       <NavBar />
       <h1 className="heading">Welcome to my first React page!</h1>
       <p className="move para">
-        If you are new to React, you should definitely check out this YouTube
-        tutorial.
+        This is the link to a list of Yotube videos that help me pass CS 3010 with an A!
       </p>
       <iframe
         className="move"
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/SqcY0GlETPk?si=KkFVj7GZ8X0ERYUN"
+        src="https://www.youtube.com/embed/videoseries?si=D3KJ11S5c3-mTS0W&amp"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -22,7 +21,7 @@ export default function Home() {
       ></iframe>
 
       <footer>
-        <p>Copyright © 2023 David Lam</p>
+        <p>Copyright © 2023 Thong Nguyen</p>
       </footer>
     </>
   );
